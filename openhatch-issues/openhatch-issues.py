42e17593dd9fdbeac02f2b71beebae3b1a7f94c8
Carol Willing <carolcode@willingconsulting.com>
2014-10-28 05:52:22 -0700

    Add hack experiment for issues and labels

--- openhatch-issues/openhatch-issues.py
+++ openhatch-issues/openhatch-issues.py
@@ -14,3 +14,17 @@ for f in gh.iter_followers():
     print(str(f))
 
 print(gh.zen())
+
+issue = gh.issue('openhatch', 'oh-mainline', 1403)
+print(issue)
+print(issue.title)
+print(issue.state)
+for l in issue.iter_labels():
+	print(str(l))
+	if 'stat' in l.name:
+		print("this is a status label")
+		if issue.is_closed() and 'resolved' not in l.name:
+			print("This issue should be marked resolved.")
+			print("Removing old status")
+			issue.remove_label(l)
+			issue.add_labels('stat:resolved')
